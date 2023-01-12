8ddce8542759704372ddc5dcefef4fd44ee05d08
Skn02 <64737065+Skn02@users.noreply.github.com>
2023-01-12 13:59:47 +0100

    Update main.cpp

--- FreeRTOS_Prjt/src/main.cpp
+++ FreeRTOS_Prjt/src/main.cpp
@@ -1,10 +1,21 @@
 #include <Arduino.h>
-// semaphores.ino
-// Practical ESP32 Multitasking
-// Binary Semaphores
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
 #define LED1_GPIO 4
 #define LED2_GPIO 5
 #define BP 12
+#define PIN_RED 23
+#define PIN_BLUE 19
+#define PIN_GREEN 18
+
+#define SCREEN_WIDTH 128        // oled display width in pixel
+#define SCREEN_HEIGHT 64
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 
 bool condition = false;
 bool taskGlobal = false;
@@ -44,6 +55,11 @@ void ledA(void *argp) {
   for (;;) { 
     digitalWrite(LED1_GPIO,digitalRead(LED1_GPIO)^1);
     delay(500);
+    
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.println("activation des moteurs...");
+    display.display();
     } 
  }
 
@@ -52,15 +68,35 @@ void ledB(void *argp) {
   for (;;) {
     digitalWrite(LED2_GPIO,digitalRead(LED2_GPIO)^1);  
     delay(250);
+ 
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.println("accélération des moteurs...");
+    display.display();
+    
   }
     
  }
- 
+
+void ledC(void *argp) {
+  
+  for (;;) {
+
+  }
+
+
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(LED1_GPIO , OUTPUT);
   pinMode(LED2_GPIO , OUTPUT);
   pinMode(BP,INPUT_PULLUP);
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_BLUE, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+
+
 
   digitalWrite(LED1_GPIO , HIGH);
   digitalWrite(LED2_GPIO , HIGH);
@@ -84,4 +120,11 @@ void setup() {
 // Not used
 void loop() {
  vTaskDelete(nullptr); 
+
+ /*
+ if BP ...
+ analogWrite(PIN_RED, 255);
+ analogWrite(PIN_BLUE, 0);
+ analogWrite(PIN_GREEN, 0);
+ */ 
  }
